1170deb1eda6930eeb7bdab3d74ca3228703437a
SPC <SPC@33-LAB402>
2025-03-28 10:29:44 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace BaiKiemTra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTien = new System.Windows.Forms.TextBox();
+            this.txtLaisuat = new System.Windows.Forms.TextBox();
+            this.txtGui = new System.Windows.Forms.TextBox();
+            this.txtDu = new System.Windows.Forms.TextBox();
+            this.txtLai = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnBang = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(242, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tính Lãi Suất Ngân Hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "TIền gốc";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "lãi suất";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "số tháng gửi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Số dư";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 309);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "tiền lãi";
+            // 
+            // txtTien
+            // 
+            this.txtTien.Location = new System.Drawing.Point(175, 103);
+            this.txtTien.Name = "txtTien";
+            this.txtTien.Size = new System.Drawing.Size(158, 20);
+            this.txtTien.TabIndex = 2;
+            // 
+            // txtLaisuat
+            // 
+            this.txtLaisuat.Location = new System.Drawing.Point(175, 144);
+            this.txtLaisuat.Name = "txtLaisuat";
+            this.txtLaisuat.Size = new System.Drawing.Size(100, 20);
+            this.txtLaisuat.TabIndex = 2;
+            // 
+            // txtGui
+            // 
+            this.txtGui.Location = new System.Drawing.Point(175, 197);
+            this.txtGui.Name = "txtGui";
+            this.txtGui.Size = new System.Drawing.Size(100, 20);
+            this.txtGui.TabIndex = 2;
+            // 
+            // txtDu
+            // 
+            this.txtDu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtDu.Location = new System.Drawing.Point(175, 249);
+            this.txtDu.Name = "txtDu";
+            this.txtDu.Size = new System.Drawing.Size(158, 20);
+            this.txtDu.TabIndex = 2;
+            // 
+            // txtLai
+            // 
+            this.txtLai.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.txtLai.Location = new System.Drawing.Point(175, 302);
+            this.txtLai.Name = "txtLai";
+            this.txtLai.Size = new System.Drawing.Size(158, 20);
+            this.txtLai.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(281, 147);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "%/năm";
+            // 
+            // btnBang
+            // 
+            this.btnBang.Location = new System.Drawing.Point(206, 341);
+            this.btnBang.Name = "btnBang";
+            this.btnBang.Size = new System.Drawing.Size(75, 23);
+            this.btnBang.TabIndex = 4;
+            this.btnBang.Text = "Tính lãi suất";
+            this.btnBang.UseVisualStyleBackColor = true;
+            this.btnBang.Click += new System.EventHandler(this.btnBang_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 410);
+            this.Controls.Add(this.btnBang);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtLai);
+            this.Controls.Add(this.txtDu);
+            this.Controls.Add(this.txtGui);
+            this.Controls.Add(this.txtLaisuat);
+            this.Controls.Add(this.txtTien);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tính Lãi Suất Ngân Hàng";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTien;
+        private System.Windows.Forms.TextBox txtLaisuat;
+        private System.Windows.Forms.TextBox txtGui;
+        private System.Windows.Forms.TextBox txtDu;
+        private System.Windows.Forms.TextBox txtLai;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnBang;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiKiemTra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBang_Click(object sender, EventArgs e)
+        {        
+               double tien = double.Parse(txtTien.Text);
+               double laisuat = double.Parse(txtLaisuat.Text); 
+               double gui = double.Parse(txtGui.Text);
+            double sodu = tien;
+            double lai = (laisuat / 12) / 100;
+               double tong = 0;
+            for (int i = 1; i <= gui; i++) {
+                tong = tien * lai;
+                tien += tong;
+                
+            }            
+            double laitong = tien - sodu;
+            txtLai.Text = Math.Round(laitong , 2).ToString();
+            txtDu.Text = Math.Round(tien , 2).ToString();
+            
+        }
+    }
+}
